86e9c58f2f747e4d78b07aac7488a8a225c8af45
LeKimThoa <68886842+LeKimThoa@users.noreply.github.com>
2022-05-14 21:11:37 +0700

    Design

--- EVinSchool_PTUD/GUI/FrmSchool.cs
+++ EVinSchool_PTUD/GUI/FrmSchool.cs
@@ -9,10 +9,12 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using BusinessLogicLayer;
 using DataAccessLayer;
+using Bunifu.UI.WinForms;
+using Bunifu.Utils;
 
 namespace GUI
 {
-    public partial class FrmSchool : Form
+    public partial class FrmSchool : BunifuForm
     {
         TeacherBUS teacherBUS = new TeacherBUS();
         public FrmSchool()
@@ -22,8 +24,38 @@ namespace GUI
 
         private void FrmSchool_Load(object sender, EventArgs e)
         {
-            List<Teacher> teachers = new TeacherBUS().GetAll();
-            gvTeacher.DataSource = teachers;
+            //List<Teacher> teachers = new TeacherBUS().GetAll();
+            //gvTeacher.DataSource = teachers;
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void bunifuTileButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bnf_Dashbroad_Click(object sender, EventArgs e)
+        {
+            bunifuPages1.PageIndex = 0;
+        }
+
+        private void bnf_Register_Click(object sender, EventArgs e)
+        {
+            bunifuPages1.PageIndex = 1;
+        }
+
+        private void bnf_Login_Click(object sender, EventArgs e)
+        {
+            bunifuPages1.PageIndex = 2;
+        }
+
+        private void txtID_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
